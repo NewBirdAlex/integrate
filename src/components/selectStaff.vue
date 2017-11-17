1b50fdfa88f4c1f40f7e373cfa74b86805698f7a
NewBirdAlex <xxqken@163.com>
2017-11-17 23:49:07 +0800

    add interface

---
+++ src/components/selectStaff.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<style scoped lang="less">
+    @import "../assets/css/common.less";
+
+</style>
+<script>
+    
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
