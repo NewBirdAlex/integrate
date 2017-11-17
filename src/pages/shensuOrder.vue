95736ddfd0fb88f8895ce04e25050fdf98b0b436
NewBirdAlex <xxqken@163.com>
2017-11-17 13:56:35 +0800

    add interface

--- src/pages/shensuOrder.vue
+++ src/pages/shensuOrder.vue
@@ -34,7 +34,7 @@
                 <span class="fs36">{{detail.userName}}</span>
                 <input type="text" class="fr fs36 marginTop" placeholder="输入积分" value="80" style="border: none;width:3rem;background: none;text-align: right;outline: none">
             </div>
-            <choosePeople v-for="(item,index) in peopleList" :name="item.userName"
+            <choosePeople v-if="getRange" v-for="(item,index) in peopleList" :name="item.userName"
                           :key="index" :point="item.selectAddScore" :range="scoreRange"
                           :ind="index"
                           :head="item.userAvatar"
@@ -42,47 +42,48 @@
                           @changePoint="changePoint">
                 <span @click="delPerson(index)" class="marginLeft"><i class="icon iconfont icon-shanchu fs36 gray" ></i></span>
             </choosePeople>
-
-
-            <!--<div class="bgWhite paddingAll lh40 fs28" v-if="getRange">-->
-                <!--<strong>全选积分</strong>-->
-                <!--<span class="gray">(选择可批量修改申请的积分)</span>-->
-                <!--<span class="fr marginRight cl" :class="{'border':!selAll}" @click="selAll=!selAll"><i class="icon iconfont icon-gouxuan blue fs36" v-if="selAll"></i></span>-->
-            <!--</div>-->
-
             <!--选择员工-->
-            <chooseStaff  @getData="accept" v-if="getRange"></chooseStaff>
+            <chooseStaff  @getData="accept" v-if="getRange" :hide=true></chooseStaff>
 
 
             <!--审批人-->
-            <div class="marginTop">
-                <subTitle :content="'审批人'" :subWord="''" :need="true"></subTitle>
-                <div class="paddingAll overflow bgWhite tac fs28">
-                    <div class="spr overflow fl marginBottom" v-for="(item,index) in approveUserList" :key="index">
-                        <div class="ps fl">
-                            <img :src="item.userAvatar" class="headPicture" alt="">
-                            <p class="marginTop" v-html="item.userName"></p>
-                        </div>
-                        <div class="pt fl marginTop gray marginLeft marginRight">
-                            .......
-                        </div>
+            <div class="marginTop"></div>
+            <subTitle :content="'审批人'" :subWord="''" :need="false"></subTitle>
+            <div class="progress ">
+                <div class="prog_list" v-for="item in detail.approveUserList">
+                    <div class="time">
+                        <i class="icon iconfont icon-gouxuan blue" v-if="item.checkStatus==0||item.checkStatus==2"></i>
+                        <i class="icon iconfont icon-wait gray" v-if="item.checkStatus==1"></i>
+                        <i class="icon iconfont icon-reject red" v-if="item.checkStatus==3"></i>
                     </div>
-                    <div class="spr overflow fl marginBottom" v-for="(item,index) in shenpiList" :key="index">
-                        <div class="ps fl">
-                            <img :src="item.userAvatar" class="headPicture" alt="">
-                            <p class="marginTop" v-html="item.userName"></p>
+                    <div class="content bgWhite paddingAll">
+                        <div class="left"><img :src="item.userAvatar" class="headPicture" alt=""></div>
+                        <div class="right">
+                            <p class="overflow">
+                                {{item.userName}}
+                                <span class="green fr" v-if="$route.params.type==1">审批通过</span>
+                                <span class="red fr" v-if="$route.params.type==2">审批不通过</span>
+
+                            </p>
+                            <p class="overflow">
+                                <span class="gray">{{item.departmentName}}</span>
+                                <span class="gray fr">{{item.approveDate}}</span>
+                            </p>
+                            <div class="triangle-left"></div>
                         </div>
-                        <div class="pt fl marginTop gray marginLeft marginRight">
-                            .......
+                        <div class="recommend">
+                            <p class="borderTop" v-if="item.context">
+                                {{item.context}}
+                            </p>
+                            <div class="rec_img" v-if="item.pics">
+                                <img :src="item" alt="" v-for="item in item.pics.split(',')">
+                            </div>
                         </div>
-                    </div>
-                    <div class="add fl" @click="shenpi" v-if="!approveUserList">
-                        <i class="icon iconfont icon-jia gray"></i>
                     </div>
                 </div>
             </div>
             <!--抄送-->
-            <div class="marginTop">
+            <div class="">
                 <subTitle :content="'抄送'" :subWord="''" :need="false"></subTitle>
                 <div class="paddingAll overflow bgWhite tac fs28">
                     <div class="spr overflow fl marginBottom"  v-for="(item,index) in chaosongList" :key="index" v-if="item">
@@ -106,7 +107,9 @@
 <style scoped lang="less">
     @import "../assets/css/common.less";
     /*@import "../assets/font/font1/iconfont.css";*/
+    .hideChooseStaff{
 
+    }
 
     .wrap2{
         position: relative;
@@ -149,7 +152,72 @@
         margin-left: 0.2rem;
         margin-right: 0.2rem;
     }
-
+    .progress{
+        background: #efeff4;
+        .paddingAll;
+        .prog_list{
+            overflow: hidden;
+            padding-bottom: 0.2rem;
+            .time{
+                width: 0.58rem;
+                float: left;
+                height: 0.2rem;
+                i{
+                    margin-top: 0.6rem;
+                    display: inline-block;
+                }
+            }
+            .content{
+                float: left;
+                width: 6.1rem;
+                .left{
+                    display: inline-block;
+                    img{
+                        width: 0.9rem;
+                        width: 0.9rem;
+                        border-radius: 50%;
+                    }
+                }
+                .right{
+                    display: inline-block;
+                    width: 5rem;
+                    vertical-align: top;
+                    text-indent: 0.2rem;
+                    line-height: 0.4rem;
+                    font-size: @fs30;
+                    position: relative;
+                    .triangle-left{
+                        position: absolute;
+                        left:-1.3rem;
+                        top:0.4rem;
+                    }
+                    p{
+                        padding-top: 0.05rem;
+                        &:nth-child(2){
+                            font-size: @fs28;
+                            span{font-size: @fs24;}
+                        }
+                    }
+                }
+                .recommend{
+                    margin-top: 0.2rem;
+                    p{
+                        font-size: @fs30;
+                        line-height: 0.4rem;
+                        padding-top: 0.1rem;
+                    }
+                    .rec_img{
+                        overflow: hidden;
+                        img{
+                            width: 0.8rem;
+                            height: 0.8rem;
+                            margin:0.1rem 0.1rem;
+                        }
+                    }
+                }
+            }
+        }
+    }
     .ps{
         width: 1.1rem;
     }
@@ -266,6 +334,7 @@
                 console.log(msg )
                 localStorage.setItem('chaosong',true)
                 document.getElementById('selectPeople').click();
+                console.log(document.getElementById('selectPeople'))
             },
             accept(data){
                 // accpet  staff person
